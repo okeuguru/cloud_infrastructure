f22ce8da8b1c427a80389976c086bf01a68f9464
Oke Uguru <ondukwe@gmail.com>
2025-02-17 13:08:59 -0500

    Update documentation

--- aws/terraform/modules/s3/s3.tf
+++ aws/terraform/modules/s3/s3.tf
@@ -4,24 +4,6 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
-//resource "aws_s3_bucket_ownership_controls" "this" {
-//  for_each = aws_s3_bucket.this
-//
-//  bucket = aws_s3_bucket.this[each.key].id
-//  rule {
-//    object_ownership = "BucketOwnerPreferred"
-//  }
-//}
-//
-//resource "aws_s3_bucket_acl" "example" {
-//  for_each = aws_s3_bucket.this
-//
-//  depends_on = [aws_s3_bucket_ownership_controls.this]
-//
-//  bucket = aws_s3_bucket.this[each.key].id
-//  acl    = "private"
-//}
-
 resource "aws_s3_bucket_policy" "this" {
   for_each = aws_s3_bucket.this
 
